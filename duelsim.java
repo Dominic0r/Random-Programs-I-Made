2d210008c51ad5747a314767b24c37ceb8af9db3
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-04-11 11:34:57 +0800

    Update duelsim.java

--- duelsim.java
+++ duelsim.java
@@ -5,31 +5,42 @@ public class Main
     public static class Person{
         String name;
         String color;
-        // static - never changes in round 
-        // dynamic - changes in round
-        int aim; // static 
+        // static - never changes throughout tournament
+        // fluid - changes throughout tournament
+        // dynamic - changes in rounds 
         
-        //statuses that affect their aim
-        int calmness; // dynamic
-        int stress; // static 
-        boolean isInjured // static 
+        int baseAim; // static - how well this person aims
+        int personality; // static - their personality number
+        int temper; // static - how much they can control their own emotions
         
-        //statuses that affect others aim 
-        int wins; // affects opponent calmness | static
         
-        //statuses that affect whether they shoot or not 
-        int mercy; // static 
-        Map<Person, Integer> relations = new HashMap<>(); // static 
-        int debt; // static 
+        int kindness; // fluid - how kind this person is 
+        int health; // fluid - how healthy they are, may be affected by injuries and such.
+        int wins; // fluid - increases the more wins they have
+        Map<Person, Integer> relations = new HashMap<>(); // fluid - their relationships with other contestants
+        Map<Person, Integer> debt = new HashMap<>(); // fluid - whether they owe a person or not
+        int calmness; // fluid - changes
+        int sanity; // fluid
         
-        //personality statuses with no effect
-        int personality; // static
         
-        int trauma; // static
+        int mercy; // dynamic - their mercy for a certain person 
+        int nerves; // dynamic - increases the more steps taken and for every failed calmness check
         
-        public Person(String name, String color, int aim,){
+        public Person(String name, String color, int baseAim, int personality, int temper, int kindness, int calmness, int sanity){
+            this.name=name;
+            this.color = color;
+            this.baseAim=baseAim;
+            this.personality=personality;
+            this.temper=temper;
+            this.kindness=kindness;
+            this.calmness=calmness;
+            this.sanity=sanity;
+            
+            this.health=100;
+            this.wins=0;
             
         }
+        
     }
     
 	public static void main(String[] args) {
